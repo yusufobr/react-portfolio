c843f83a41459b77f64aed74d59ddd84b020cb7f
yusufobr <yousarsif80@gmail.com>
2023-06-11 01:11:40 +0100

    add animation to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import ContactBtns from "./ContactBtns";
+import { Fade } from "react-awesome-reveal";
 
 function Footer() {
   return (
-    <div id="contact" className="container max-w-screen-xl mx-auto px-4 ">
-      <div className="flex  flex-col justify-between h-full footerGradien p-6 rounded-xl mb-8">
-        <div className="flex flex-col gap-3">
-          <span className="text-3xl font-semibold">Want to work together?</span>
-          <div>
-            <p>
-              Feel free to reach out for collaborations or just a friendly hello
-            </p>
-            <p>youssef.oubaari@gmail.com</p>
+    <Fade triggerOnce>
+      <div id="contact" className="container max-w-screen-xl mx-auto px-4 ">
+        <div className="flex  flex-col justify-between h-full footerGradien p-6 rounded-xl mb-8">
+          <div className="flex flex-col gap-3">
+            <span className="text-3xl font-semibold">Want to work together?</span>
+            <div>
+              <p>
+                Feel free to reach out for collaborations or just a friendly hello
+              </p>
+              <p>youssef.oubaari@gmail.com</p>
+            </div>
           </div>
+          <ContactBtns />
         </div>
-        <ContactBtns />
       </div>
-    </div>
+    </Fade>
   );
 }
 
