1c120f959344c465ab04ed63b17a329a558a5c40
yusufobr <yousarsif80@gmail.com>
2023-06-11 01:04:48 +0100

    add fade animation to projects cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import data from "./dammyData";
 import Project from "./Project";
+import { Fade } from "react-awesome-reveal";
 
 function Projects() {
   return (
-    <div id="projects" className="container max-w-screen-xl px-4 mx-auto grid grid-cols-2 gap-4">
-      {data.map((item) => (
-        <Project
-          title={item.title}
-          des={item.des}
-          img={item.img}
-          bg={item.bg}
-          live={item.live}
-          code={item.code}
-        />
-      ))}
+    <div
+      id="projects"
+      className="container max-w-screen-xl px-4 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-4"
+    >
+      <Fade cascade>
+        {data.map((item) => (
+          <Project
+            title={item.title}
+            des={item.des}
+            img={item.img}
+            bg={item.bg}
+            live={item.live}
+            code={item.code}
+          />
+        ))}
+      </Fade>
     </div>
   );
 }
