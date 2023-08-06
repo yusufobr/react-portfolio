010834084b0de6fae5ba9262bbf8b6b56010c414
Youssef Oubari <yousarsif80@gmail.com>
2023-08-06 12:42:55 +0000

    Update VCardButton.jsx

--- src/components/VCardButton.jsx
+++ src/components/VCardButton.jsx
@@ -3,13 +3,14 @@ import { saveAs } from 'file-saver';
 
 const generateVCard = () => {
   // Replace the sample data with your actual vCard information
-  const vCardData = `BEGIN:VCARD
-                    VERSION:3.0
-                    FN:Youssef OUBARI
-                    TEL:+212-648-125-896
-                    EMAIL:youssef.oubaari@gmail.com
-                    END:VCARD`;
-
+  const vCardData = `
+BEGIN:VCARD
+VERSION:3.0
+FN:Youssef OUBARI
+N:OUBARI;Youssef;;;
+TEL;TYPE=WORK,VOICE:0648125896
+EMAIL:youssef.oubaari@gmail.com
+END:VCARD` ;
   return new Blob([vCardData], { type: 'text/vcard' });
 };
 
